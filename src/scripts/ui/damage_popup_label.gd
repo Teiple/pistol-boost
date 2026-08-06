98d3ecc2e1e4037c579563b880462e176f533587
teiple <teiple022@gmail.com>
2026-08-07 05:43:30 +0700

    in mid of smth

--- src/scripts/ui/damage_popup_label.gd
+++ src/scripts/ui/damage_popup_label.gd
@@ -1,2 +1,9 @@
 class_name DamagePopupLabel
 extends Label
+
+@export var _player_hit_color : Color = Color.RED
+@export var _enemy_hit_color : Color = Color.WHITE
+@export var _label_scene : PackedScene
+
+func _ready() -> void:
+	ControlPool.new(50, _label_scene)
